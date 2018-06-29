948f0e6534f450323b463c7accfa50d393913c57
mashaniy <40450254+mashaniy@users.noreply.github.com>
2018-06-29 08:43:10 +0700

    Update ann.js

--- ann.js
+++ ann.js
@@ -4,7 +4,7 @@ const config = require("./config.json"); // kita akan menaruh prefix dan token d
 
 bot.on("ready", async () => {
 	console.log(`${bot.user.username} Sudah online!`);
-	bot.user.setActivity("For Girl", {type: "PLAY HM-"});	
+	bot.user.setActivity("HM.ForGirl", {type: "PLAYING"});	
 });
 
 bot.on("message", async message => {
